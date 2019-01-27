e52d375954c6e32cc221e5ea889fdf4702dea998
Jason Lee <jsn0716@gmail.com>
2019-01-26 19:41:24 -0800

    Render TabsPage

--- WebApp/idscan/src/FrontPage.js
+++ WebApp/idscan/src/FrontPage.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 
 import Header from './Header.js';
-import TabsPage from './TabsPage.js'
+import TabsPage from './components/TabsPage.js'
+
+var student_list = [
+{student_name: "Jason", student_no: 1},
+{student_name: "Andy", student_no: 2},
+{student_name: "Stephen", student_no: 3}
+];
+
+var exam_dates = [5];
 
 class FrontPage extends Component {
     constructor(props) {
@@ -17,7 +25,7 @@ class FrontPage extends Component {
         return (
             <div id = "front-page">
                 <Header/>
-                <TabsPage/>
+                <TabsPage exam_dates={exam_dates} student_list={student_list}/>
             </div>
         )
     }
